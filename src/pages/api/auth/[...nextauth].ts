1caa3f148c89573095ac87bf0bf56158e1f1750d
danvb112 <danielbonasser@gmail.com>
2022-05-30 06:26:26 -0300

    Connect with faunadb database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,33 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import {fauna} from '../../../services/fauna';
+import {query as q} from 'faunadb';
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,      
-    }),
+    }), 
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user.user);
+      const { email } = user.user;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), 
+          {
+            data: {email}
+          }
+        ));
+  
+        return true;
+      } catch {
+        return false;
+      }
+
+
+  },
+}})
